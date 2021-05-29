ded1f55c64b55e1ba1888f515f225269849a41bc
Neeraj R <neerajrk007@gmail.com>
2021-05-30 01:59:44 +0530

    Receiving User input usiong TextField widget and using onChanged is added

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
 import './transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  @override
+  String titleInput;
+  String amountInput;
   final List<Transaction> transactions = [
     Transaction(
       id: 't1',
-      title: 'travel',
+      title: 'Travel',
       amount: 100,
       date: DateTime.now(),
     ),
     Transaction(
       id: 't2',
       title: 'New Shoe',
-      amount: 500,
+      amount: 50.29,
       date: DateTime.now(),
     )
   ];
+  @override
   Widget build(BuildContext conext) {
     return MaterialApp(
       home: Scaffold(
@@ -28,7 +31,7 @@ class MyApp extends StatelessWidget {
             title: Text('Expense App'),
           ),
           body: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              //mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
                 Container(
@@ -37,6 +40,34 @@ class MyApp extends StatelessWidget {
                     child: Text('Graph Card'),
                     color: Colors.blue,
                     elevation: 5,
+                  ),
+                ),
+                Card(
+                  elevation: 5,
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: <Widget>[
+                          TextField(
+                              decoration: InputDecoration(labelText: 'Title'),
+                              onChanged: (val) {
+                                titleInput = val;
+                              }),
+                          TextField(
+                            decoration: InputDecoration(labelText: 'Amount'),
+                            onChanged: (val) => amountInput = val,
+                          ),
+                          TextButton(
+                              style: ButtonStyle(
+                                  foregroundColor:
+                                      MaterialStateProperty.all(Colors.purple)),
+                              child: Text('Add Transaction'),
+                              onPressed: () {
+                                print(titleInput);
+                                print(amountInput);
+                              }),
+                        ]),
                   ),
                 ),
                 Column(
@@ -50,16 +81,30 @@ class MyApp extends StatelessWidget {
                         ),
                         decoration: BoxDecoration(
                             border: Border.all(
-                          color: Colors.black,
+                          color: Colors.purple,
                           width: 2,
                         )),
-                        child: Text(tx.amount.toString()),
-                        padding:EdgeInsets.all(10)
+                        padding: EdgeInsets.all(10),
+                        child: Text('\$${tx.amount}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple)),
                       ),
-                      Column(children: <Widget>[
-                        Text(tx.title),
-                        Text(tx.date.toString()),
-                      ]),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              tx.title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
+                              ),
+                            ),
+                            Text(DateFormat.yMMMd().format(tx.date),
+                                style: TextStyle(
+                                    fontSize: 15, color: Colors.grey)),
+                          ]),
                     ]),
                   );
                 }).toList())
